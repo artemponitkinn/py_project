8b309b983a599692691f685e8f3c28286954dfc0
artemponitkinn <artem.ponitkin@gmail.com>
2023-12-26 22:26:04 +0300

    full

--- funcs.py
+++ funcs.py
@@ -1,5 +1,13 @@
-def sum(a, b) -> int:
-	return(a+b)
 
-def subtraction(a, b) -> int:
-	return(a-b)
+def sum():
+	a = 5
+	b = 10
+	suma = a + b
+
+	return print(suma)
+
+def subtraction():
+	a = 5
+	b = 10
+	subtractions = a - b
+	return print(subtractions)
---
+++ main.py
@@ -0,0 +1,7 @@
+from funcs sum
+from funcs subtraction
+def main():
+    print(sum)
+    print(subtraction)
+if __name__ == '__main__':
+    main()
